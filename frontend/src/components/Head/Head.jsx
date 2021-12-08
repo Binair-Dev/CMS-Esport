d635f5c0d1e9ef5bbee31b9434b730d01e043198
Brian <van.bellinghen.brian@gmail.com>
2021-12-08 20:51:51 +0100

    author added

--- frontend/src/components/Head/Head.jsx
+++ frontend/src/components/Head/Head.jsx
@@ -11,7 +11,7 @@ class Header extends Component {
                 <meta charset="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
                 <meta name="description" content="" />
-                <meta name="author" content="" />
+                <meta name="author" content="B_nair" />
                 <title>CMS Esport - Basic Theme</title>
                 <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
                 <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossorigin="anonymous"></script>
